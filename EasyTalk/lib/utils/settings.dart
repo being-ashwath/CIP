4bd51c9d80a06c44ec974b9290e131edad919c76
being-ashwath <ashwathganesh619@gmail.com>
2021-07-14 00:23:36 +0530

    EasyTalk Application

---
+++ EasyTalk/lib/utils/settings.dart
@@ -0,0 +1,2 @@
+const APP_ID = "17d1fcd612974109af6befee6c219674";
+const Token = "00617d1fcd612974109af6befee6c219674IAALPZhhr7LO71+/J++FOuVBW9lmwo4hqNdzzlxuKy7KeQY/sfkAAAAAEABZxLUY6bCsYAEAAQDosKxg";
